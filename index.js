e8de338eda8c8e1dadb16f78f6257a0ed3fbbef5
syed Muhammad Ali <164505979+Syed-MuhammadAli@users.noreply.github.com>
2024-03-24 17:25:53 +0500

    created guessing game
    guessing game created with chalk and published to NPM

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+//-------------//
+// 1. Computer will generate ramdome number
+// 2. User input for guessing number
+// 3. Compare both generated numbers
+// 4. Print result
+//-------------//
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number: ",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.green("You guessed the correct number!"));
+}
+else {
+    console.log(chalk.red("You guessed the wrong number!"));
+}
+console.log(randomNumber);
